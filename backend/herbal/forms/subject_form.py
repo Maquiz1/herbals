d94860ab124048345c2833b1ec819203db162556
Maquiz1 <manquiz92@gmail.com>
2026-03-19 04:45:51 +0300

    first commit

--- backend/herbal/forms/subject_form.py
+++ backend/herbal/forms/subject_form.py
@@ -1,45 +1,61 @@
 from django import forms
 from herbal.models import Subject
-from locations.models import Region,District,Ward
+from locations.models import Region, District, Ward
+
 
 class SubjectForm(forms.ModelForm):
 
     class Meta:
         model = Subject
 
-        fields = [
-            "reg_date",
-            "first_name",
-            "last_name",
-            "sex",
-            "dob",
-            "region",
-            "district",
-            "ward",
-        ]
+        exclude = ["subject_id", "site"]  # auto + calculated
 
+        labels={
+            "reg_date":"Registration Date",
+            "dob":"Date",
+            "sex":"Sex",
+            "hid":"Hospital ID",
+            "idn":"ID Number",
+        }
         widgets = {
             "reg_date": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
-            "first_name": forms.TextInput(attrs={"class": "form-control"}),
-            "last_name": forms.TextInput(attrs={"class": "form-control"}),
             "dob": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
-            "sex": forms.Select(attrs={"class": "form-control"}),
+            "age": forms.NumberInput(attrs={"class": "form-control"}),
 
-            # 👇 IMPORTANT (we control via AJAX)
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "middle_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+
+            "sex": forms.Select(attrs={"class": "form-control"}),
+            "id_type": forms.Select(attrs={"class": "form-control"}),
+            "marital_status": forms.Select(attrs={"class": "form-control"}),
+            "education_level": forms.Select(attrs={"class": "form-control"}),
+            "occupation": forms.Select(attrs={"class": "form-control"}),
+
+            "hid": forms.TextInput(attrs={"class": "form-control"}),
+            "idn": forms.TextInput(attrs={"class": "form-control"}),
+
+            "phone_number": forms.TextInput(attrs={"class": "form-control"}),
+            "other_phone": forms.TextInput(attrs={"class": "form-control"}),
+
+            "street": forms.TextInput(attrs={"class": "form-control"}),
+            "remarks": forms.Textarea(attrs={"class": "form-control", "rows": 3}),
+
+            # Select2 fields
             "region": forms.Select(attrs={"class": "form-control", "id": "region"}),
             "district": forms.Select(attrs={"class": "form-control", "id": "district"}),
             "ward": forms.Select(attrs={"class": "form-control", "id": "ward"}),
         }
-        
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # 👇 IMPORTANT: prevent loading all data
+        # AJAX-controlled fields
         self.fields["region"].queryset = Region.objects.none()
         self.fields["district"].queryset = District.objects.none()
         self.fields["ward"].queryset = Ward.objects.none()
-        
-        # 🔥 FIX: when form is submitted
+
+        # FIX for POST (validation)
         if self.data:
             region_id = self.data.get("region")
             district_id = self.data.get("district")
@@ -51,8 +67,11 @@ class SubjectForm(forms.ModelForm):
             if district_id:
                 self.fields["ward"].queryset = Ward.objects.filter(district_id=district_id)
 
-        # 🔥 FIX: when editing existing object
+        # FIX for edit mode
         elif self.instance.pk:
-            self.fields["region"].queryset = Region.objects.filter(id=self.instance.region_id)
-            self.fields["district"].queryset = District.objects.filter(region=self.instance.region)
-            self.fields["ward"].queryset = Ward.objects.filter(district=self.instance.district)
+            if self.instance.region:
+                self.fields["region"].queryset = Region.objects.filter(id=self.instance.region_id)
+                self.fields["district"].queryset = District.objects.filter(region=self.instance.region)
+
+            if self.instance.district:
+                self.fields["ward"].queryset = Ward.objects.filter(district=self.instance.district)
